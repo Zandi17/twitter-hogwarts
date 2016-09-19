71a8bbe34db9685d22ea4b6f1b03f49d26162d56
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-09-19 17:23:51 -0500

    refactorizar el codigo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("boton");
 	boton.addEventListener("click", function(e) {
-	e.preventDefault();
-	var txt = document.getElementById("txt").value;
-	document.getElementById("txt").value = "";
+	e.preventDefault(); // esta funcion es para q mi txtarea no vaya a mi url
+	var txtArea = document.getElementById("txt");
+	agregarMensaje(txtArea.value);
+	txtArea.value =" ";
+	
+	});
+	function agregarMensaje(texto){
 	var pub =document.createElement("p");
-	pub.innerText = txt;
+	pub.innerText = texto;
 	var contenedor = document.getElementById("contenedor");
 	contenedor.insertBefore(pub,contenedor.childNodes[0]);
-	});
+	document.getElementById("txt").value = "";
+	}
 });
