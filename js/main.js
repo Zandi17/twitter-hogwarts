9fc66bbd5b75c99caf826bb9c247d0f75b8896a1
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-09-20 10:26:09 -0500

    boton desabilitado y contador

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("boton");
+	var txtArea = document.getElementById("txt");
+	var contador = document.getElementById("contar");
+	var caracteres = contador.innerHTML = 140;
 	boton.addEventListener("click", function(e) {
 	e.preventDefault(); // esta funcion es para q mi txtarea no vaya a mi url
-	var txtArea = document.getElementById("txt");
 	agregarMensaje(txtArea.value);
 	txtArea.value =" ";
-	
+	boton.disabled = true;
+	contador.innerHTML = 140;
 	});
+	// funcion
 	function agregarMensaje(texto){
 	var pub =document.createElement("p");
 	pub.innerText = texto;
@@ -14,4 +18,16 @@ window.addEventListener("load", function() {
 	contenedor.insertBefore(pub,contenedor.childNodes[0]);
 	document.getElementById("txt").value = "";
 	}
+	// nuevo evento
+	txtArea.addEventListener("keydown",function(){
+	boton.disabled = false;
+	var long = txtArea.value.length;
+
+	if ( long <= caracteres){
+		contador.innerHTML = caracteres - long;
+	}else {
+		contador.innerHTML = caracteres - long;
+	}
+	});
+
 });
